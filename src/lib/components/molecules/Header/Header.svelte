e9a51b0de61924e28755749eefc848ef6bbad2d9
Aleksander Grygier <alek@newfantastic.com>
2022-03-11 11:53:27 +0100

    feat: Essential styles + Header UI

---
+++ src/lib/components/molecules/Header/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Logo from "$lib/components/atoms/Logo/Logo.svelte";
+</script>
+
+<header class="header">
+	<Logo />
+</header>
+
+<style>
+	header {
+		background: var(--c-primary);
+		display: grid;
+		min-height: var(--header-height);
+		place-items: center;
+	}
+
+	header :global(.logo) {
+		height: 3rem;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	import "$lib/css/index.css";
+</script>
+
+<script>
+	import Header from "$lib/components/molecules/Header/Header.svelte";
+</script>
+
+<Header />
+
+<slot />
+
+<style>
+	:global(.header) {
+		left: 0;
+		position: fixed;
+		right: 0;
+		top: 0;
+		z-index: var(--layer-2);
+	}
+</style>
