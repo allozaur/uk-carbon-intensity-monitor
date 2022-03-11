6ce654c20ea59c36f6873d0a1405fc3c4d42974f
Aleksander Grygier <alek@newfantastic.com>
2022-03-11 11:55:41 +0100

    feat: Utils functions

---
+++ src/lib/utils/date.js
@@ -0,0 +1,29 @@
+export const formatDate = (date, locale = "en-GB") => {
+  date = new Date(date)
+
+  if (locale = "en-GB") {
+    date = date.toLocaleDateString("en-GB")
+  }
+
+  return date
+}
+
+export const formatTime = (date, locale = "en-GB") => {
+  date = new Date(date)
+
+  if (locale = "en-GB") {
+    date = date.toLocaleTimeString("en-GB")
+  }
+
+  return date
+}
+
+export const parseTime = (from, to) => {
+  if (formatDate(from) === formatDate(to))
+    return `${formatDate(from)} ${formatTime(
+      from
+    )} - ${formatTime(to)}`;
+  return `${formatDate(from)} ${formatTime(
+    from
+  )} - ${formatDate(to)} ${formatTime(to)}`;
+};
---
+++ src/lib/utils/index.js
@@ -0,0 +1,9 @@
+import { formatDate, formatTime, parseTime } from "./date";
+import { toKebabCase } from "./to-kebab-case";
+
+export {
+  formatDate,
+  formatTime,
+  parseTime,
+  toKebabCase
+}
